ac746f93472ac068485449088fe927060ffdc773
Lília Paula <liliaxtz2@gmail.com>
2021-12-09 19:36:20 -0300

    creat server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log("hello word 1")
+import http from 'http'
+
+import express from 'express'
+
+
+const app = express()
+
+app.use(express.json())
+
+//sem express
+// const server = http.createServer((request, response) =>{
+//     response.statusCode = 200
+//     response.setHeader('Content-Type', 'text/plain')
+//     response.end('Funcionou 🙃')
+// })
+
+const port = 5000
+
+app.get('/', (request, response) => {
+    response.status(200).json({
+        message: "criado 😎"
+    })
+})
+
+app.listen(port, () => {
+    console.log('Server secees port:  ', port)
+})
+
+// server.listen(port, () => {
+//     console.log('server funcionando na porta: ', port , '😎')
+// })
